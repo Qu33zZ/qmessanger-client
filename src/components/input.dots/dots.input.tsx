c5a198c9c70a84fdf69e0927e4fead5c2db9f4e6
QueeezZ <uince.games@gmail.com>
2022-09-13 22:54:45 +0300

    dots input bug fixes

---
+++ src/components/input.dots/dots.input.tsx
@@ -0,0 +1,57 @@
+import React, {ChangeEvent, CompositionEvent, useRef} from 'react';
+import "./dots.input.styles.css";
+
+interface IDotsInputProps{
+	value:string;
+	setValue:React.Dispatch<React.SetStateAction<string>>;
+	inputsCount?:number;
+};
+
+const DotsInput:React.FC<IDotsInputProps> = ({value, setValue, inputsCount = 6}) => {
+	const inputParentRef = useRef<HTMLDivElement | null>(null);
+
+	const handleInput = (e:CompositionEvent<HTMLInputElement>) => {
+		e.preventDefault();
+		const currentInput = e.currentTarget;
+		const numberRegexp = new RegExp(/[0-9]{1}/gi);
+		if(!numberRegexp.test(e.data)) return;
+		if(!inputParentRef.current) return e.preventDefault();
+
+		const inputs = Array.from(inputParentRef.current?.children);
+		const index = inputs.indexOf(e.currentTarget);
+
+		if(index >= inputs.length-1) return;
+
+		setValue(prevState => `${prevState}${e.nativeEvent.data}`);
+
+		if(currentInput.value.length === 0) currentInput.value += e.nativeEvent.data;
+		else if(currentInput.value.length >= 1){
+			const nextInput = inputs[index+1] as HTMLInputElement;
+			nextInput.value = e.nativeEvent.data;
+			if(index < inputs.length-1) nextInput.focus();
+		};
+	};
+
+	const handleDelete = (e:ChangeEvent<HTMLInputElement>) =>{
+		if(e.currentTarget.value.length > 0) return;
+		if(!inputParentRef.current) return;
+
+		const inputs = Array.from(inputParentRef.current?.children);
+		const index = inputs.indexOf(e.currentTarget);
+
+		setValue((prevCode) => prevCode.slice(0, -1));
+
+		if(index === 0) return;
+
+		(inputs[index-1] as HTMLInputElement).focus();
+	};
+
+
+	return (
+		<div className={"dots-input"} ref={inputParentRef}>
+			{Array(inputsCount).fill(Math.random).map((item, index) => <input placeholder={'•'} maxLength={2} type={"number"} className={"dot-input"} onBeforeInput={handleInput} onChange={handleDelete} key={index}/>)}
+		</div>
+	);
+};
+
+export default DotsInput;
--- src/pages/phone.confirm/phone.confirm.page.tsx
+++ src/pages/phone.confirm/phone.confirm.page.tsx
@@ -3,38 +3,13 @@ import womenSit from "../../assets/women_sit.png";
 import boySit from "../../assets/boy_sit.png";
 import BaseButton from "../../ui/base.button/base.button";
 import "./phone.confirm.styles.css";
+import DotsInput from "../../components/input.dots/dots.input";
 
 const PhoneConfirmPage = () => {
 	const [code, setCode] = useState<string>("");
-	const inputRef = useRef<HTMLDivElement | null>(null);
+
 
 	useEffect(() => console.log(code), [code])
-
-	const handleInput = (e:CompositionEvent<HTMLInputElement>) => {
-		e.preventDefault();
-		const currentInput = e.currentTarget;
-
-		if(!inputRef || !inputRef.current) return e.preventDefault();
-
-		const inputs = Array.from(inputRef.current?.children);
-		const index = inputs.indexOf(e.currentTarget);
-
-		if(index >= inputs.length-1) return;
-
-		setCode(prevState => `${prevState}${e.nativeEvent.data}`);
-
-		if(currentInput.value.length === 0) currentInput.value += e.nativeEvent.data;
-		else if(currentInput.value.length >= 1){
-			const nextInput = inputs[index+1] as HTMLInputElement;
-			nextInput.value = e.nativeEvent.data;
-			if(index < inputs.length-1) nextInput.focus();
-		}
-
-		if(currentInput.value.length === 0){
-			setCode(prevState => `${prevState.slice(0, -1)}`);
-			if(index > 0) (inputs[index-1] as HTMLInputElement).focus();
-		};
-	};
 
 	return (
 		<div className={"base-page"}>
@@ -42,16 +17,8 @@ const PhoneConfirmPage = () => {
 			<p className={"phone-description-text"}><span>Enter confirmation code</span><br/>We have sent you an SMS with the code to +62 1309 - 1710 - 1920</p>
 			<img src={womenSit} alt="Women" className={"women"}/>
 			<img src={boySit} alt="Boy" className={"boy"}/>
-			<div className={"phone-confirm-code--area"}>
-				<div className={"dots-input"} ref={inputRef}>
-					<input placeholder={'•'} maxLength={2} type={"number"} className={"dot-input"} onBeforeInput={handleInput}/>
-					<input placeholder={'•'} maxLength={2} type={"number"} className={"dot-input"} onBeforeInput={handleInput}/>
-					<input placeholder={'•'} maxLength={2} type={"number"} className={"dot-input"} onBeforeInput={handleInput}/>
-					<input placeholder={'•'} maxLength={2} type={"number"} className={"dot-input"} onBeforeInput={handleInput}/>
-					<input placeholder={'•'} maxLength={2} type={"number"} className={"dot-input"} onBeforeInput={handleInput}/>
-					<input placeholder={'•'} maxLength={2} type={"number"} className={"dot-input"} onBeforeInput={handleInput}/>
-				</div>
-
+			<div className={"phone-input-area"}>
+				<DotsInput value={code} setValue={setCode}/>
 			</div>
 			<BaseButton
 				style={{margin:"auto auto 115px auto"}}
