c7045297e9ee3d8a3407a2785747fa70b7aaa2fa
QueeezZ <uince.games@gmail.com>
2022-09-25 14:18:49 +0300

    create user store

---
+++ src/store/user.store.ts
@@ -0,0 +1,20 @@
+import { makeAutoObservable } from "mobx";
+import { IUser } from "../interfaces/IUser";
+
+class UserStore{
+	user:IUser | null = null;
+	constructor() {
+		makeAutoObservable(this, {}, {deep:true});
+	}
+
+	login(user:IUser){
+		this.user = user;
+	}
+
+	logout(){
+		this.user = null;
+	}
+
+}
+
+export default new UserStore();
