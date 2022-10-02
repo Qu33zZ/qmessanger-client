f75ec5621381833072f1a47feb9580e6ded5f5e2
QueeezZ <uince.games@gmail.com>
2022-10-02 12:35:13 +0300

    fix chats api service getChats method return type

--- src/api/services/chats.service.ts
+++ src/api/services/chats.service.ts
@@ -1,10 +1,11 @@
 import { IChat } from "../../interfaces/IChat";
 import { $authorizedApi } from "../axios.auth.instance";
+import { IChatFromAPI } from "../../interfaces/IChatFromAPI";
 
 class ChatsService{
-	async getChats():Promise<IChat[] | null>{
+	async getChats():Promise<IChatFromAPI[] | null>{
 		try{
-			const chatsResponse = await $authorizedApi.get<IChat[]>("/chats");
+			const chatsResponse = await $authorizedApi.get<IChatFromAPI[]>("/chats");
 			if(chatsResponse.status === 200){
 				return chatsResponse.data;
 			}
