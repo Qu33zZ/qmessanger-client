cd3df847f1adafa93d224cfc58594bff72d46ea3
QueeezZ <uince.games@gmail.com>
2022-11-13 01:45:14 +0200

    fix/edit server url in the config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const CONFIG = {
 	api:{
-		server_url:"http://localhost:5000",
+		server_url:"https://qmessanger-api.tk",
 		rest_base_endpoint:"/api",
 		static_base_endpoint:"/static",
 		socket_base_endpoint:"/"
